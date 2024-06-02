18142b26142fa9838c0ff7e4afc07d5f6af9bdae
NuReach <nureach46@gmail.com>
2024-06-03 01:34:04 +0700

    bento grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { ParallaxScrollDemo } from "./components/ui/ParallaxScrollDemo";
 import RoomOne from "./components/ui/RoomOne";
 import RoomTwo from "./components/ui/RoomTwo";
 import Footer from "./components/ui/Footer";
+import { BentoGridDemo } from "./components/ui/BentoGridDemo";
 export default function Home() {
   return (
     <main className="">
@@ -24,16 +25,16 @@ export default function Home() {
         <div className="absolute top-32 px-6 md:top-60 lg:w-[600px] lg:left-36 w-full">
           <TypeWritten />
         </div>
-        <div className="absolute w-full flex justify-center top-48 md:top-80 lg:w-[600px]  lg:top-[410px] lg:left-36  text-white">
+        <div className="absolute w-full flex justify-center top-52 md:top-80 lg:w-[600px]  lg:top-[410px] lg:left-36  text-white">
           <div className="flex flex-col justify-center items-center">
             <p className="text-center text-xs px-6 md:text-lg">Booking your stay at our resort is as easy as a breeze</p>
             <div className="mt-3">
-              <button className="p-3 px-6 bg-black rounded-lg hover:scale-105 transition text-sm shadow-sm shadow-white">Book Now</button>
+              <button className="p-3 px-6  bg-black rounded-lg hover:scale-105 transition text-xs md:text-sm shadow-sm shadow-white">Book Now</button>
             </div>
           </div>
         </div>
       </section>
-      <section className="p-9">
+      <section className="p-3">
         <div className="font-bold text-3xl flex w-full justify-center items-center gap-3">
           <div className="w-full h-1 bg-black"></div>
           <p>Room</p>
@@ -44,14 +45,25 @@ export default function Home() {
           <RoomTwo />
         </div>
       </section>
-      <section className="p-9">
-        <div className="font-bold text-3xl flex w-full justify-center items-center gap-3">
+      <section className="p-3">
+        <div className="font-bold text-lg md:text-3xl flex w-full justify-center items-center gap-3">
           <div className="w-full h-1 bg-black"></div>
-          <p>Photo</p>
+          <p>Photos</p>
           <div className="w-full h-1 bg-black"></div>
         </div>
         <ParallaxScrollDemo />
       </section>
+      <section className="p-3">
+        <div className="font-bold text-lg md:text-3xl flex w-full justify-center items-center gap-3">
+          <div className="w-full h-1 bg-black"></div>
+          <p>Activities</p>
+          <div className="w-full h-1 bg-black"></div>
+        </div>
+        <div>
+          <BentoGridDemo />
+        </div>
+      </section>
+      
       <Footer />
     </main>
   );
