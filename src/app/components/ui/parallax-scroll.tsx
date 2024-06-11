98c642c8bde8b5d35ae73bd22109259870aa3b05
NuReach <nureach46@gmail.com>
2024-06-12 01:00:26 +0700

    change photo

--- src/app/components/ui/parallax-scroll.tsx
+++ src/app/components/ui/parallax-scroll.tsx
@@ -45,8 +45,8 @@ export const ParallaxScroll = ({
             >
               <Image
                 src={el}
-                className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
-                height="400"
+                className="h-96 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                height="600"
                 width="400"
                 alt="thumbnail"
               />
@@ -58,7 +58,7 @@ export const ParallaxScroll = ({
             <motion.div style={{ y: translateSecond }} key={"grid-2" + idx}>
               <Image
                 src={el}
-                className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                className="h-96 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
                 height="400"
                 width="400"
                 alt="thumbnail"
@@ -71,7 +71,7 @@ export const ParallaxScroll = ({
             <motion.div style={{ y: translateThird }} key={"grid-3" + idx}>
               <Image
                 src={el}
-                className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                className="h-96 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
                 height="400"
                 width="400"
                 alt="thumbnail"
